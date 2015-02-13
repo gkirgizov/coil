0a68e84ad1dc9b0ec1f46bcd8edc7eadb5be86e3
Gregory Kirgizov <gkirgizov@gmail.com>
2015-02-13 18:43:15 +0300

    First task about factorial is done.

---
+++ PrimarySolution1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class SimpleMeth
+    {
+        public static uint FactorialI(int calcThis)
+        {
+            uint result = 1;
+            for (uint n = 1; n <= calcThis; ++n)
+            {
+                result *= n;
+            }
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
