fb3e70d6e9b1bc29b272fb8f04f07621281ddc74
Gregory Kirgizov <gkirgizov@gmail.com>
2015-03-18 21:09:37 +0300

    Task 5 is done

---
+++ 2 semester/PrimarySolution1/Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventLoop = new ArrowEventLoop();
+            var arrowMover = new ConsoleArrowMover();
+
+            eventLoop.Initialize += arrowMover.Initialize;
+            eventLoop.LeftArrowHandler += arrowMover.MoveLeft;
+            eventLoop.RightArrowHandler += arrowMover.MoveRight;
+            eventLoop.UpArrowHandler += arrowMover.MoveUp;
+            eventLoop.DownArrowHandler += arrowMover.MoveDown;
+
+            eventLoop.Run();
+        }
+    }
+}
---
+++ 2 semester/PrimarySolution1/Tests/Task5/FunctionsTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task5;
+
+namespace Tests.Task5
+{
+    [TestClass]
+    public class FunctionsTest
+    {
+        private List<int> list;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = new List<int> { 1, 2, 3 };
+        }
+
+        [TestMethod]
+        public void MapTest()
+        {
+            var newList = Functions.Map(list, x => ++x);
+            for (var i = 0; i < 3; ++i)
+            {
+                Assert.AreEqual(++list[i], newList[i]);
+            }
+        }
+
+        [TestMethod]
+        public void FilterTest()
+        {
+            var newList = Functions.Filter(list, item => (item % 2) == 1);
+            Assert.IsFalse(newList.Contains(2));
+            Assert.IsTrue(newList.Contains(1));
+            Assert.IsTrue(newList.Contains(3));
+        }
+
+        [TestMethod]
+        public void FoldTest()
+        {
+            var startValue = 1;
+            var res = Functions.Fold(list, startValue, (value, item) => value + item);
+            var testRes = startValue;
+            foreach(var item in list)
+            {
+                testRes += item;
+            }
+            Assert.AreEqual(testRes, res);
+        }
+    }
+}
