fe4d426b21f8bf62d9308d0085e1a0f2d46d6091
Gregory Kirgizov <gkirgizov@gmail.com>
2015-04-03 14:59:05 +0300

    Control work #1 is done. QueuePriority.cs is added with unit tests

---
+++ 2 semester/PrimarySolution1/ControlWork1/QueuePriority.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlWork1
+{
+    public class QueuePriority<T>
+    {
+        public QueuePriority()
+        {
+            this.Size = 0;
+        }
+
+        public QueuePriority(T data, int priority)
+        {
+            this.tail = new QueuePriorityItem<T>(data, priority);
+            this.head = this.tail;
+            this.Size = 1;
+        }
+
+        public int Size { get; private set; }
+
+        public void Enqueue(T data, int priority)
+        {
+            var newItem = new QueuePriorityItem<T>(data, priority);
+
+            if (this.head == null)
+            {
+                this.tail = newItem;
+                this.head = this.tail;
+            }
+            else
+            {
+                if (this.head.Next == null)
+                { 
+                    if (priority > this.head.Priority)
+                    {
+                        newItem.Next = this.tail;
+                        this.head = newItem;
+                    }
+                    else
+                    {
+                        this.tail = newItem;
+                        this.head.Next = this.tail;
+                    }
+                }
+                else
+                {
+                    var ptr = this.head;
+                    while (ptr.Next != this.tail && priority <= ptr.Next.Priority)
+                    {
+                        ptr = ptr.Next;
+                    }
+                    newItem.Next = ptr.Next;
+                    ptr.Next = newItem;
+                }
+            }
+            ++this.Size;
+        }
+
+        public T Dequeue()
+        {
+            if (this.Size == 0)
+            {
+                throw new EmptyQueueException();
+            }
+
+            var returned = this.head.Data;
+            this.head = this.head.Next;
+            --this.Size;
+            return returned;
+        }
+
+        private QueuePriorityItem<T> head = null;
+        private QueuePriorityItem<T> tail = null;
+
+        private class QueuePriorityItem<T>
+        {
+            public QueuePriorityItem(T data, int priority)
+            {
+                this.Data = data;
+                this.Priority = priority;
+            }
+
+            public T Data { get; set; }
+
+            public int Priority { get; set; }
+
+            public QueuePriorityItem<T> Next { get; set; }
+        }
+    }
+}
