eb2d77c1188b8d0f904c6ab9051eccbca8b18dff
gkirgizov <gkirgizov@gmail.com>
2016-02-21 15:40:35 +0300

    Homework1

---
+++ 4 semester/hw1/nn1-2.fsx
@@ -0,0 +1,12 @@
+﻿
+let rec fact x =
+    match x with
+    | 1 -> 1
+    | x when x < 1 -> 1
+    | _ -> x * fact (x - 1)
+
+let rec fibb n =
+    match n with
+    | 0L -> 0L
+    | 1L -> 1L
+    | _ -> fibb (n - 1L) + fibb (n - 2L)
