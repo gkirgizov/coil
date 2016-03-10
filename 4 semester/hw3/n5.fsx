b8a0f505571558f1e4d5f0348817bddf326ee39b
gkirgizov <gkirgizov@gmail.com>
2016-03-10 17:21:32 +0300

    hw3

---
+++ 4 semester/hw3/n5.fsx
@@ -0,0 +1,12 @@
+﻿//#5
+let gen =
+    let isPrime n = 
+        let rec check i =
+            (i > n/2) || (n % i <> 0 && check (i+1))
+        check 2
+    
+    let numbers = Seq.initInfinite(fun i -> i + 2)
+    seq { for n in numbers do if isPrime n then yield n }
+
+//test : first 100 primes
+let first100primes = gen |> Seq.take 100 |> Seq.toList;;
---
+++ 4 semester/hw3/nn1-2.fsx
@@ -0,0 +1,20 @@
+﻿//#1
+let getMaxSumOfPair ls =
+    let rec getMaxSumOfPair_ ls max maxi i =
+        match ls with
+        | [] -> maxi
+        | h :: t ->
+            match t with
+            | ht :: tt when ht + h > max -> getMaxSumOfPair_ t (ht + h) (i + 1) (i + 1)
+            | ht :: tt -> getMaxSumOfPair_ t max maxi (i + 1)
+            | _ -> maxi
+    getMaxSumOfPair_ ls 0 0 0
+
+//#2
+let nof = List.filter (fun x -> x % 2 = 0) >> List.length
+
+//let nof' = List.map (fun x -> (x + 1) % 2) >> List.sum 
+//there is no List.sum in the list of allowed functions in the task, so ...
+let nof' = List.map (fun x -> (x + 1) % 2) >> List.fold (fun acc x -> acc + x) 0
+
+let nof'' = List.fold (fun acc x -> acc + (x + 1) % 2) 0
