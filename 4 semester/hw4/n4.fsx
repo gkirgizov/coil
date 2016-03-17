f7e7999d31cd6fb6019f8d5bfb97068a9e8c7863
gkirgizov <gkirgizov@gmail.com>
2016-03-18 00:10:19 +0300

    hw4: nn:1,2,4

---
+++ 4 semester/hw4/n4.fsx
@@ -0,0 +1,56 @@
+﻿
+//#4
+type Op<'a> = 
+    | Sum of ('a -> 'a -> 'a)
+    | Sub of ('a -> 'a -> 'a)
+    | Mul of ('a -> 'a -> 'a)
+    | Div of ('a -> 'a -> 'a)
+
+type Eq<'a> =
+    | Monom of 'a * 'a //Coefficient and degree
+    | Op of Op<'a>
+
+let normalize leq =
+    let rec norm leq acc =
+        match leq with
+        | Monom(c, d) as m :: Op(f) :: t ->
+            match f with
+            | Sum(f) -> norm t <| m :: acc
+            | Sub(f) -> 
+                match t with
+                | Monom(c', d') :: t' -> norm t' <| Monom(-c', d') :: m :: acc
+                | _ -> None
+            | Mul(f) ->
+                match t with
+                | Monom(c', d') :: t' -> norm (Monom(c * c', d + d') :: t') acc
+                | _ -> None
+            | Div(f) ->
+                match t with
+                | Monom(c', d') :: t' -> norm (Monom(c / c', d - d') :: t') acc
+                | _ -> None
+        | Monom(c, d) as m :: [] -> Some(List.rev <| m :: acc)
+        | [] -> Some(List.rev acc)
+        | _ -> None
+    norm leq []
+
+let derMonomial (neutralBySum, neutralByMul) m =
+    match m with 
+    | Monom(c, d) when d = neutralBySum -> None
+    | Monom(c, d) when c = neutralBySum-> None
+    | Monom(c, d) -> Some(Monom(c * d, d - neutralByMul))
+    | Op(f) as op -> None
+
+let derMonomialInt = derMonomial (0, 1)
+
+let derPolynomialInt = 
+    normalize
+    >> Option.get
+    >> List.map derMonomialInt
+    >> List.filter Option.isSome
+    >> List.fold (fun acc m -> m :: acc) []
+    >> List.rev
+
+//some examples
+let t = [ Monom(1, 0); Op(Sum(+)); Monom(2, 1); Op(Sub(-)); Monom(3, 100) ]
+let t' = [ Monom(1, 0); Op(Mul(*)); Monom(2, 1); Op(Mul(*)); Monom(3, 100) ]
+let t'' = [ Monom(6, 1); Op(Div(/)); Monom(3, 100) ]
---
+++ 4 semester/hw4/nn1-2.fsx
@@ -0,0 +1,31 @@
+﻿    
+//#1
+let checkstring checkingPairs (s : string) = 
+    let checkchar (c : char) (openchar, closechar) = 
+        match c with
+        | сс when c = openchar -> (+) 1
+        | сс when c = closechar -> (+) -1
+        | _ -> fun x -> x
+    
+    let checkchars = List.map << checkchar
+    let invoke lx lf = List.map (fun (f, x) -> f x) <| List.zip lf lx
+    let checkPairs lpairs lacc c = invoke lacc <| checkchars c lpairs
+
+    let rec checkstring_ s lacc =
+        match s with
+        | _ when List.exists ((>) 0) lacc -> false
+        | [] when List.sum lacc = 0 -> true
+        | [] -> false
+        | h :: t -> checkstring_ t <| checkPairs checkingPairs lacc h
+
+    let toCharList s = [for c in s -> c]
+    checkstring_ (toCharList s) [for i in 1 .. List.length checkingPairs -> 0]
+
+let braces = [ ('(', ')'); ('{', '}'); ('[', ']') ]
+let checkstringForBraces = checkstring braces
+
+//#2
+let func    x l = List.map (fun y -> y * x) l
+let func'   x   = List.map (fun y -> y * x)
+let func''  x   = List.map ((*) x)
+let func'''     = List.map << (*) 
