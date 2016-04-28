a2bb0d7dd5e610db441cc0ddc547ac46abd83519
gkirgizov <gkirgizov@gmail.com>
2016-04-28 23:04:17 +0300

    Work on course project is in progress

---
+++ 4 semester/db/project/populate_tables_relations2.sql
@@ -0,0 +1,59 @@
+﻿--employees
+INSERT INTO employees VALUES(8, 'Monica Washington', 'monica@work.com', 'manager');
+INSERT INTO employees VALUES(9, 'Mendy Fillipovich', 'fillipovich@work.com', 'tester');
+
+--projects
+INSERT INTO projects VALUES('messenger', '2016-03-01', '2016-11-01', 8);
+
+--employees_projects
+INSERT INTO employees_projects VALUES('messenger', 8, 100);
+INSERT INTO employees_projects VALUES('messenger', 9, 100);
+INSERT INTO employees_projects VALUES('messenger', 3, 30);
+INSERT INTO employees_projects VALUES('messenger', 5, 50);
+INSERT INTO employees_projects VALUES('messenger', 6, 30);
+INSERT INTO employees_projects VALUES('messenger', 7, 70);
+
+--builds
+INSERT INTO builds VALUES('messenger', 10);
+INSERT INTO builds VALUES('messenger', 11);
+INSERT INTO builds VALUES('messenger', 12);
+INSERT INTO builds VALUES('messenger', 20);
+
+--issues
+INSERT INTO issues VALUES('messenger', 1, 'Tabs support', '', 'feature', 'B', 3, 5);
+INSERT INTO issues VALUES('messenger', 2, 'Tabs support', 'Program crashes with many tabs open (more than 20).', 'bug', 'B', 3, 5);
+
+INSERT INTO issues VALUES('messenger', 3, 'XMPP accounts support', '', 'feature', 'A', 9, 7);
+
+INSERT INTO issues VALUES('messenger', 4, 'Changable chat background', '', 'feature', 'C', 9, 6);
+ 
+INSERT INTO issues VALUES('messenger', 5, 'VOIP calls', 'Add support of voip calls.', 'feature', 'A', 9, 7);
+INSERT INTO issues VALUES('messenger', 6, 'VOIP calls interrupts', 'Constant interrupts with weak internet signal.', 'bug', 'B', 9, 7);
+
+--bug_sources
+INSERT INTO bug_sources VALUES('messenger', 2, 1);
+INSERT INTO bug_sources VALUES('messenger', 6, 5);
+
+--issue_statuses
+INSERT INTO issue_statuses VALUES('messenger', 10, 1, 'open');
+INSERT INTO issue_statuses VALUES('messenger', 10, 3, 'open');
+INSERT INTO issue_statuses VALUES('messenger', 10, 5, 'open');
+
+INSERT INTO issue_statuses VALUES('messenger', 11, 1, 'closed');
+INSERT INTO issue_statuses VALUES('messenger', 11, 2, 'open');
+INSERT INTO issue_statuses VALUES('messenger', 11, 3, 'closed');
+INSERT INTO issue_statuses VALUES('messenger', 11, 4, 'open');
+INSERT INTO issue_statuses VALUES('messenger', 11, 5, 'closed');
+INSERT INTO issue_statuses VALUES('messenger', 11, 6, 'open');
+
+INSERT INTO issue_statuses VALUES('messenger', 12, 1, 'checked');
+INSERT INTO issue_statuses VALUES('messenger', 12, 2, 'closed');
+INSERT INTO issue_statuses VALUES('messenger', 12, 3, 'open');
+INSERT INTO issue_statuses VALUES('messenger', 12, 4, 'closed');
+INSERT INTO issue_statuses VALUES('messenger', 12, 5, 'checked');
+INSERT INTO issue_statuses VALUES('messenger', 12, 6, 'closed');
+
+INSERT INTO issue_statuses VALUES('messenger', 20, 2, 'checked');
+INSERT INTO issue_statuses VALUES('messenger', 20, 3, 'closed');
+INSERT INTO issue_statuses VALUES('messenger', 20, 4, 'open');
+INSERT INTO issue_statuses VALUES('messenger', 20, 6, 'open');
---
+++ 4 semester/db/project/views.sql
@@ -0,0 +1,57 @@
+﻿--View #1
+
+CREATE OR REPLACE VIEW last_statuses AS
+WITH last_builds AS (
+	SELECT project, MAX(version) AS build_version
+	FROM builds
+	GROUP BY project
+)
+SELECT last_builds.project, build, issue, status
+FROM issue_statuses INNER JOIN last_builds ON (build = build_version AND issue_statuses.project = last_builds.project);
+
+--View #2
+
+CREATE OR REPLACE VIEW issue_all_info AS
+SELECT issues.project, issue_uid, type, priority, status, title, description, responsible_tester, responsible_programmer
+FROM issues LEFT JOIN last_statuses ON (issue_uid = issue AND issues.project = last_statuses.project);
+
+CREATE OR REPLACE VIEW project_total_issues AS
+WITH total AS (
+	SELECT project, type, COUNT(type) as iss_total
+	FROM issue_all_info
+	GROUP BY project, type
+)
+SELECT total.project, total.type, iss_total, coalesce(iss_done, 0) as iss_done
+FROM total LEFT JOIN (
+	SELECT project, type, COUNT(type) as iss_done
+	FROM issue_all_info
+	WHERE status = 'checked' OR status IS NULL
+	GROUP BY project, type)
+	AS done ON (total.project = done.project AND total.type = done.type);
+
+CREATE OR REPLACE VIEW project_stat AS 
+WITH bugs_stat AS (
+	SELECT project, (iss_total - iss_done) as bugs_undone
+	FROM project_total_issues
+	WHERE type = 'bug'
+)
+SELECT bugs_stat.project, bugs_undone, features_done
+FROM bugs_stat JOIN (
+	SELECT project, CAST(iss_done * 100 / iss_total AS numeric(4, 1)) AS features_done
+	FROM project_total_issues
+	WHERE type = 'feature'
+) AS features_stat ON bugs_stat.project = features_stat.project;
+
+--View #3
+
+CREATE OR REPLACE VIEW emp_stat AS
+WITH emp_stat AS (
+	SELECT responsible_programmer AS programmer, COUNT(*) AS solved
+	FROM issue_all_info
+	WHERE type = 'bug' AND (status = 'checked' OR status IS NULL)
+	GROUP BY responsible_programmer
+)
+SELECT programmer, solved
+FROM emp_stat
+WHERE solved >= (SELECT AVG(solved) FROM emp_stat);
+
