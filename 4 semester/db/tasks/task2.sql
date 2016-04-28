cb2719109bf6046aa02895ec1a9a77fea9bc7f88
gkirgizov <gkirgizov@gmail.com>
2016-04-28 23:01:47 +0300

    Commiting completed tasks on db course

---
+++ 4 semester/db/tasks/task2.sql
@@ -0,0 +1,32 @@
+﻿/* Сколько вариантов ходов может король сделать? 
+   (сколько вокруг фигуры не занятых союзниками клеток?)*/
+
+CREATE OR REPLACE FUNCTION isFree(cellX int, cellY int, allie char(5))
+RETURNS boolean AS
+$$ BEGIN 
+	RETURN NOT EXISTS (
+		SELECT * FROM fboard
+		WHERE x = cellX AND y = cellY AND fcolor = allie);
+END; $$
+LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION checkWays(fid_ int)
+RETURNS int AS $$
+DECLARE
+	count int := 0;
+	kx int := (SELECT x FROM fboard WHERE fid = fid_);
+	ky int := (SELECT y FROM fboard WHERE fid = fid_);
+	kcolor char(5) := (SELECT fcolor FROM fboard WHERE fid = fid_);
+BEGIN 
+	FOR ix IN 1 .. 8 LOOP
+		FOR iy IN 1 .. 8 LOOP
+			IF abs(ix - kx) < 2 AND abs(iy - ky) < 2 AND isFree(ix, iy, kcolor) THEN
+				count := count + 1;
+			END IF;
+		END LOOP;
+	END LOOP;
+	RETURN count;
+END; $$
+LANGUAGE plpgsql;
+
+SELECT checkWays(1);
---
+++ 4 semester/db/tasks/task4.sql
@@ -0,0 +1,29 @@
+﻿/* Процедура: написать процедуру, которая делает ход фигуры. 
+   Параметры процедуры: id фигуры, координаты, куда фигура ходит, и внешний параметр - результат. 
+   Результат должен быть:
+   0, если походить не удалось, 
+   1, если походили
+   2, если съели вражескую фигуру. */
+
+CREATE OR REPLACE FUNCTION makeStep(fid_ int, x_to int, y_to int)
+RETURNS int AS $$
+DECLARE
+	result int := 0;
+	fcolor_ char(5) := (SELECT fcolor FROM fboard WHERE fid = fid_);
+BEGIN
+	IF NOT EXISTS (SELECT * FROM board WHERE fid = fid_) --если не сущ-т такой фигуры на доске ИЛИ
+	   OR (x_to < 1 OR x_to > 8 OR y_to < 1 OR y_to > 8) --если пытаемся походить за пределы доски ИЛИ
+	   OR NOT isFree(x_to, y_to, fcolor_) 		     --новая клетка занята союзником
+	THEN RETURN result; END IF;
+
+	IF EXISTS (SELECT * FROM fboard WHERE x = x_to AND y = y_to AND fcolor <> fcolor_) THEN
+		DELETE FROM board WHERE x = x_to AND y = y_to;
+		result := 1; 
+	END IF;
+		
+	UPDATE board SET x = x_to, y = y_to WHERE fid = fid_;
+	RETURN result + 1;
+END; $$
+LANGUAGE plpgsql;
+
+--SELECT makeStep(10, 50, 2);
