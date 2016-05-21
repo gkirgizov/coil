d67ee8eabb0f4c518be670652fc7422e833ce7d1
gkirgizov <gkirgizov@gmail.com>
2016-05-22 00:51:36 +0300

    hw6 : fix n1

--- 4 semester/hw6/n1.fsx
+++ 4 semester/hw6/n1.fsx
@@ -1,15 +1,19 @@
 ﻿open System
 
 type RoundingBuilder(precision : int) =
-    member self.Bind(x, f) = f x
+    member self.Bind(x : float, f) = f <| Math.Round(x, precision)
     member self.Return(x : float) = Math.Round(x, precision)
 
 let rounding u = new RoundingBuilder(u)
-
 
 // test 1
 let t1 = rounding 3 {
     let! a = 2.0 / 12.0
     let! b = 3.5
     return a / b
+}
+
+let t2 = rounding 3 {
+    let! a = 1.0 / 3000.0
+    return a * 1000.0
 }
